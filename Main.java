82b87a5a65def9af580fd2bf500bf9e7bffc8083
hafzansh <hafizansh.m@gmail.com>
2020-05-05 00:14:44 +0800

    menambahkan plane dengan contoh dari game spacewar

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        Plane player = new Plane();
+        player.planetype = "Jet";
+        player.score = 0;        
+        player.health = 3;
+        player.stage = 1;
+
+        System.out.println("Select Difficulty : Easy, Medium, Hard");
+        player.difficulty = "Hard";
+        System.out.println(player.difficulty + " Difficulty Selected");
+        System.out.println();
+
+        player.shoot();
+        player.print();
+
+        System.out.println();
+
+        player.hit();
+        player.print();
+
+        System.out.println();
+
+        player.nextstage();
+        player.hit();
+        player.shoot();
+        player.print();
+
+        System.out.println();
+
+        player.hit();
+        player.print();
+        player.gameover();
+    }
+}
---
+++ Plane.java
@@ -0,0 +1,30 @@
+public class Plane {
+    String planetype,difficulty;
+    int score,stage,health;
+
+
+    void print(){
+        System.out.println("Current Health : " + health);
+        System.out.println("Current Score : " + score);
+        System.out.println("Current Stage : " + stage);
+    }
+    void shoot(){
+        System.out.println("Asteroid Destroyed..");
+        score+=20;        
+    }
+
+    void hit(){
+        System.out.println("You got hit by asteroid, losing 1 point on health");
+        health-=1;
+    }
+
+    void gameover(){
+        System.out.println("Current health is " + health +", game over...");
+    }
+
+    void nextstage(){
+        stage++;
+        System.out.println("Entering stage " + stage);
+    }
+    
+}
